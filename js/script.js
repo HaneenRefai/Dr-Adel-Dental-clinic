fbfbff131c5b13debd343bc8b5362fb8038fb275
Sara Khater <sara.khater@future-group.com>
2023-11-18 16:36:17 +0200

    add tablet size

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.onload = function () { 
+    let slides =  
+        document.getElementsByClassName('carousel-item'); 
+  
+    function addActive(slide) { 
+        slide.classList.add('active'); 
+    } 
+  
+    function removeActive(slide) { 
+        slide.classList.remove('active'); 
+    } 
+  
+    addActive(slides[0]); 
+    setInterval(function () { 
+        for (let i = 0; i < slides.length; i++) { 
+            if (i + 1 == slides.length) { 
+                addActive(slides[0]); 
+                setTimeout(removeActive, 350, slides[i]); 
+                break; 
+            } 
+            if (slides[i].classList.contains('active')) { 
+                setTimeout(removeActive, 350, slides[i]); 
+                addActive(slides[i + 1]); 
+                break; 
+            } 
+        } 
+    }, 1500); 
+};
+
+function myFunction() {
+    var x = document.querySelector("nav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
